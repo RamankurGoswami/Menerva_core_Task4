5234415aa66cff34f3aece37d6812ef73559d17b
RamankurGoswami <gutuostad@gmail.com>
2021-06-07 19:14:06 +0530

    deploy

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CardDetail from './Components/CardDetail/CardDetail';
+
+function App() {
+  return (
+    <div className="App">
+      <CardDetail />
+    </div>
+  );
+}
+
+export default App;
